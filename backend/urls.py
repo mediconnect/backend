34c689727a8a0a8443f0132b436e1e8760300d18
Jiashen Cao <caojiashen24@gmail.com>
2018-03-21 01:07:43 -0400

    fix some naming issue

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('user/', include('customer.urls')),
+    path('customer/', include('customer.urls')),
 ]
