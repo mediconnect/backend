b8fd5c0d8309432bde4b07ef5931fd3b40f4008d
Jiashen Cao <caojiashen24@gmail.com>
2018-03-21 01:00:12 -0400

    add skeleton code for customer registration view

---
+++ customer/views.py
@@ -0,0 +1,12 @@
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class Register(APIView):
+    def get(self, request, format=None):
+        pass
+
+    def post(self, request, format=None):
+        pass
