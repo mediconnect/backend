5520ffe2657520a42cdb3bd8634e09e044a5d3b1
Arnoldosmium <eaglemewpsy@gmail.com>
2018-04-08 14:38:01 -0700

    0408 cloud computing last min

--- atlas/creator.py
+++ atlas/creator.py
@@ -1,9 +1,15 @@
+import copy
+
 def create_optional_field_serializer(Serializer):
     all_field_names = map(lambda field: field.name,
                           Serializer.Meta.model._meta.get_fields())
+    new_extra_kwargs = copy.deepcopy(Serializer.Meta.extra_kwargs or {})
+    for name in all_field_names:
+        new_extra_kwargs[name] = new_extra_kwargs.get(name, {})
+        new_extra_kwargs[name]['required'] = False
 
     class _optional_field_serializer(Serializer):
         class Meta(Serializer.Meta):
-            extra_kwargs = {}
+            extra_kwargs = new_extra_kwargs
 
     return _optional_field_serializer
