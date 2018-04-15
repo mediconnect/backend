f9bc30bf2d6d4ed10b59cc90a1f01ccd3cd2211a
Arnoldosmium <eaglemewpsy@gmail.com>
2018-04-15 07:53:44 -0700

    0415 add more decorator

--- atlas/guarantor.py
+++ atlas/guarantor.py
@@ -5,6 +5,7 @@ Guarantor decorator
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
 
+
 def use_serializer(Serializer, pass_in='auto', many=False):
     def _decorator(func):
         def wrapper(self, request, *args, **kwargs):
@@ -19,5 +20,24 @@ def use_serializer(Serializer, pass_in='auto', many=False):
         return wrapper
     return _decorator
 
-def on_raise_response():
-    pass
+
+def on_exception_response(exception_or_list, status=400):
+    if isinstance(exception_or_list, type):
+        all_exceptions = (exception_or_list, )
+    else:
+        all_exceptions = tuple(exception_or_list)
+
+    def _decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except all_exceptions as e:
+                return JsonResponse({
+                    'error': type(e).__name__,
+                    'detail': str(e)
+                }, status=status)
+        return wrapper
+    return _decorator
+
+
+any_exception_throws_400 = on_exception_response(Exception)
--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from atlas.guarantor import use_serializer
+from atlas.guarantor import use_serializer, any_exception_throws_400
 from atlas.locator import AModule
 from rest_framework.views import APIView
 from .serializers import CompleteReservationSerializer, CreateReservationSerializer, ReservationSerializer
@@ -11,34 +11,33 @@ reservation_module = AModule()
 @reservation_module.route("create", name="reservation_init")
 class InitialCreate(APIView):
 
+    @any_exception_throws_400
     @use_serializer(Serializer=CreateReservationSerializer)
     def put(self, serializer, format=None):
         created = serializer.create(serializer.data)
         new_reservation = Reservation.objects.create(**created)
-        return JsonResponse({'rid': new_reservation['id']}, status=200)
+        return JsonResponse({'rid': new_reservation['id']})
 
 @reservation_module.route(r"(?<resid>.+?)/update", name="reservation_update")
 class Update(APIView):
 
+    @any_exception_throws_400
     @use_serializer(Serializer=CompleteReservationSerializer)
     def post(self, serializer, resid, format=None):
-        try:
-            reservation = Reservation.objects.get(id=resid)
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
+        reservation = Reservation.objects.get(id=resid)
 
         for attr, value in serializer.data.items():
-            try:
-                reservation[attr].set(value)
-            except Exception as e:
-                return JsonResponse({'error': str(e)}, status=400)
+            reservation[attr].set(value)
 
         reservation.save()
-        return JsonResponse({'updated_fields': serializer.data.keys()}, status=200)
+        return JsonResponse({'updated_fields': serializer.data.keys()})
 
 
 @reservation_module.route(r"(?<resid>.+?)/info", name="reservation_get")
 class GetReservationInfo():
 
-    def get(self):
-        pass
+    @any_exception_throws_400
+    def get(self, request, resid, format=None):
+        reservation = Reservation.objects.get(id=resid)
+
+        return JsonResponse(ReservationSerializer(reservation).data)
