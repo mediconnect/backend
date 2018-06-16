16009ba472389b90650a7a3d3b656976176f6d57
Jiashen Cao <caojiashen24@gmail.com>
2018-06-16 11:33:01 -0400

    add tests case for register exception and profile

--- customer/tests.py
+++ customer/tests.py
@@ -4,14 +4,14 @@ import copy
 from .util import test_general
 
 
-class RegisterExceptionHandlingTestCase(TestCase):
-    """ Test customer registration exception. """
+class RegisterTestCase(TestCase):
+    """ Test normal customer registration procedural. """
 
     data = {
-        'email': 'exception_test@test.com',
+        'email': 'register@test.com',
         'password': '/.,buz123',
         'password_confirmation': '/.,buz123',
-        'first_name': 'fuck',
+        'first_name': 'do not fuck',
         'last_name': 'me',
         'tel': '123456789',
         'address': 'the test is awesome'
@@ -20,7 +20,11 @@ class RegisterExceptionHandlingTestCase(TestCase):
     url = '/register/'
 
     def setUp(self):
-        Customer.objects.filter(user__email='exception_test@test.com').delete()
+        Customer.objects.filter(user__email='register@test.com').delete()
+
+    def test_normal_register(self):
+        status, body = test_general(self.url, self.data, 'post')
+        self.assertEqual(status, 200)
 
     def test_fields_blank_exception(self):
         request = copy.deepcopy(self.data)
@@ -43,33 +47,11 @@ class RegisterExceptionHandlingTestCase(TestCase):
         self.assertEqual(status, 400)
 
 
-class NormalRegisterTestCase(TestCase):
-    """ Test normal customer registration procedural. """
-
-    data = {
-        'email': 'normal_test@test.com',
-        'password': '/.,buz123',
-        'password_confirmation': '/.,buz123',
-        'first_name': 'do not fuck',
-        'last_name': 'me',
-        'tel': '123456789',
-        'address': 'the test is awesome'
-    }
-    url = '/register/'
-
-    def setUp(self):
-        Customer.objects.filter(user__email='normal_test@test.com').delete()
-
-    def test_normal_register(self):
-        status, body = test_general(self.url, self.data, 'post')
-        self.assertEqual(status, 200)
-
-
-class NormalLoginTestCase(TestCase):
+class LoginTestCase(TestCase):
     """ Test customer normal login. """
 
     data = {
-        'email': 'normal_test@test.com',
+        'email': 'login@test.com',
         'password': '/.,buz123',
         'password_confirmation': '/.,buz123',
         'first_name': 'do not fuck',
@@ -77,11 +59,58 @@ class NormalLoginTestCase(TestCase):
         'tel': '123456789',
         'address': 'the test is awesome'
     }
-    url = '/register/'
+    url = '/login/'
 
     def setUp(self):
-        Customer.objects.filter(user__email='normal_test@test.com').delete()
-        test_general(self.url, self.data, 'post')
+        Customer.objects.filter(user__email='login@test.com').delete()
+        test_general('/register/', self.data, 'post')
 
     def test_normal_login(self):
+        request = {
+            'email': 'login@test.com',
+            'password': '/.,buz123'
+        }
+        status, body = test_general(self.url, request, 'post')
+        self.assertEqual(status, 200)
+
+    def test_wrong_password_login(self):
+        request = {
+            'email': 'login@test.com',
+            'password': '/.,buz'
+        }
+        status, body = test_general(self.url, request, 'post')
+        self.assertEqual(status, 400)
+
+    def test_user_not_exist_login(self):
+        request = {
+            'email': 'login_none@test.com',
+            'password': '/.,buz'
+        }
+        status, body = test_general(self.url, request, 'post')
+        self.assertEqual(status, 400)
+
+
+class ProfileTestCase(TestCase):
+    """ Test customer profile. """
+
+    data = {
+        'email': 'profile@test.com',
+        'password': '/.,buz123',
+        'password_confirmation': '/.,buz123',
+        'first_name': 'do not fuck',
+        'last_name': 'me',
+        'tel': '123456789',
+        'address': 'the test is awesome'
+    }
+    url = '/profile/'
+
+    def setUp(self):
+        Customer.objects.filter(user__email='profile@test.com').delete()
+        test_general('/register/', self.data, 'post')
+
+    def test_normal_profile_info_retrieve(self):
+        status, body = test_general(self.url, None, 'get')
+        self.assertEqual(status, 200)
+
+    def test_normal_profile_info_update(self):
         pass
