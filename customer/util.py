1685107af1f3b19c3d35466899e2e7b7a85a60cc
Jiashen Cao <caojiashen24@gmail.com>
2018-08-01 16:54:24 +0800

    0801 fix customer module testing error

--- customer/util.py
+++ customer/util.py
@@ -1,12 +1,12 @@
-from django.test import Client
+from rest_framework.test import APIClient
 
 
 def test_general(url, request, method):
-    c = Client()
+    client = APIClient()
     if method == 'post':
-        response = c.post(url, request)
+        response = client.post(url, request, format='json')
     elif method == 'get':
-        response = c.get(url, request)
+        response = client.get(url, request, format='json')
     else:
-        response = c.put(url, request)
-    return response.status_code, response
+        response = client.put(url, request, format='json')
+    return response.status_code, response.content
