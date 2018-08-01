fff73d05d848516d2fe674110f6881bf51bd0d07
Jiashen Cao <caojiashen24@gmail.com>
2018-08-01 16:54:24 +0800

    0801 fix customer module testing error

--- customer/tests.py
+++ customer/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from customer.models import Customer
 import copy
 from .util import test_general
@@ -7,17 +8,23 @@ from .util import test_general
 class RegisterTestCase(TestCase):
     """ Test normal customer registration procedural. """
 
-    data = {
+    auth = {
         'email': 'register@test.com',
         'password': '/.,buz123',
         'password_confirmation': '/.,buz123',
-        'first_name': 'do not fuck',
-        'last_name': 'me',
+        'first_name': 'register',
+        'last_name': 'test'
+    }
+    customer = {
         'tel': '123456789',
         'address': 'the test is awesome'
     }
+    data = {
+        'auth': auth,
+        'customer': customer,
+    }
     fields = ['email', 'password', 'password_confirmation', 'first_name', 'last_name', 'tel', 'address']
-    url = '/register/'
+    url = reverse('customer_register')
 
     def setUp(self):
         Customer.objects.filter(user__email='register@test.com').delete()
@@ -29,10 +36,11 @@ class RegisterTestCase(TestCase):
     def test_fields_blank_exception(self):
         request = copy.deepcopy(self.data)
         for field in self.fields:
-            request.pop(field, None)
+            key = 'auth' if field in self.auth else 'customer'
+            request[key].pop(field, None)
             status, body = test_general(self.url, request, 'post')
             self.assertEqual(status, 400)
-            request.update({field: self.data[field]})
+            request[key].update({field: self.data[key][field]})
 
     def test_email_exist_exception(self):
         request = copy.deepcopy(self.data)
@@ -42,7 +50,7 @@ class RegisterTestCase(TestCase):
 
     def test_password_mismatch_exception(self):
         request = copy.deepcopy(self.data)
-        request['password_confirmation'] = '/.,buz'
+        request['auth']['password_confirmation'] = '/.,buz'
         status, body = test_general(self.url, request, 'post')
         self.assertEqual(status, 400)
 
@@ -51,19 +59,23 @@ class LoginTestCase(TestCase):
     """ Test customer normal login. """
 
     data = {
-        'email': 'login@test.com',
-        'password': '/.,buz123',
-        'password_confirmation': '/.,buz123',
-        'first_name': 'do not fuck',
-        'last_name': 'me',
-        'tel': '123456789',
-        'address': 'the test is awesome'
+        'auth': {
+            'email': 'login@test.com',
+            'password': '/.,buz123',
+            'password_confirmation': '/.,buz123',
+            'first_name': 'login',
+            'last_name': 'test'
+        },
+        'customer': {
+            'tel': '123456789',
+            'address': 'the test is awesome'
+        }
     }
-    url = '/login/'
+    url = reverse('customer_login')
 
     def setUp(self):
         Customer.objects.filter(user__email='login@test.com').delete()
-        test_general('/register/', self.data, 'post')
+        test_general(reverse('customer_register'), self.data, 'post')
 
     def test_normal_login(self):
         request = {
@@ -94,23 +106,28 @@ class ProfileTestCase(TestCase):
     """ Test customer profile. """
 
     data = {
-        'email': 'profile@test.com',
-        'password': '/.,buz123',
-        'password_confirmation': '/.,buz123',
-        'first_name': 'do not fuck',
-        'last_name': 'me',
-        'tel': '123456789',
-        'address': 'the test is awesome'
+        'auth': {
+            'email': 'profile@test.com',
+            'password': '/.,buz123',
+            'password_confirmation': '/.,buz123',
+            'first_name': 'profile',
+            'last_name': 'test'
+        },
+        'customer': {
+            'tel': '123456789',
+            'address': 'the test is awesome'
+        }
     }
-    url = '/profile/'
+    url = reverse('customer_profile')
 
     def setUp(self):
         Customer.objects.filter(user__email='profile@test.com').delete()
-        test_general('/register/', self.data, 'post')
+        test_general(reverse('customer_register'), self.data, 'post')
+        self.id = Customer.objects.get(user__email='profile@test.com').id
 
     def test_normal_profile_info_retrieve(self):
-        status, body = test_general(self.url, None, 'get')
-        self.assertEqual(status, 200)
+        # TODO: Retrieve data with customer id.
+        pass
 
     def test_normal_profile_info_update(self):
         pass
