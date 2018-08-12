f3dd3fe0472d81ec9bffba69e6645106b4d99246
Jiashen Cao <caojiashen24@gmail.com>
2018-08-12 10:35:57 +0800

    0812 fix search test case

--- search/tests.py
+++ search/tests.py
@@ -4,19 +4,23 @@ from disease.models import Disease
 from hospital.models import Hospital
 from rank.models import Rank
 from rest_framework.test import APIClient
+import json
 
 
 class SearchTestCase(TestCase):
     url = reverse('search_hospital')
 
     def setUp(self):
+        Disease.objects.all().delete()
+        Hospital.objects.all().delete()
+        Rank.objects.all().delete()
         disease = Disease.objects.create(name='精神', keyword='精神，精，神')
         hospital = Hospital.objects.create(name='aaa')
         Rank.objects.create(disease=disease, hospital=hospital)
 
-    def normal_search_test(self):
+    def test_normal_search(self):
         client = APIClient()
-        response = client.get(self.url + 'query=精')
+        response = client.get(self.url + '?query=精')
         self.assertEqual(response.status_code, 200)
 
         disease_data = [
@@ -32,25 +36,33 @@ class SearchTestCase(TestCase):
         ]
 
         for disease_entry in disease_data:
+            disease = Disease.objects.create(name=disease_entry['name'], keyword=disease_entry['keyword'])
             for i, hospital_entry in enumerate(hospital_data):
-                disease = Disease.objects.create(name=disease_entry['name'], keyword=disease_entry['keyword'])
                 hospital = Hospital.objects.create(name=hospital_entry['name'])
                 Rank.objects.create(rank=i, disease=disease, hospital=hospital)
 
-        response = client.get(self.url + 'query=一')
+        response = client.get(self.url + '?query=一')
         self.assertEqual(response.status_code, 200)
-        data = response.body
+        data = json.loads(response.content)
         self.assertEqual(len(data['hospitals']), 0)
         self.assertEqual(len(data['diseases']), 3)
 
-        response = client.get(self.url + 'query=一一')
+        response = client.get(self.url + '?query=一一')
         self.assertEqual(response.status_code, 200)
-        data = response.body
+        data = json.loads(response.content)
         self.assertEqual(len(data['hospitals']), 0)
         self.assertEqual(len(data['diseases']), 3)
 
-        response = client.get(self.url + 'query=一一一')
+        response = client.get(self.url + '?query=一一一')
         self.assertEqual(response.status_code, 200)
-        data = response.body
+        data = json.loads(response.content)
         self.assertEqual(len(data['hospitals']), 0)
         self.assertEqual(len(data['diseases']), 3)
+
+        hospitals_url = reverse('search_hospital_by_disease')
+        for disease_entry in disease_data:
+            response = client.get(hospitals_url + '?id=' + str(Disease.objects.get(name=disease_entry['name']).id))
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.content)
+            self.assertEqual(len(data['hospitals']), 3)
+            self.assertEqual(len(data['diseases']), 0)
